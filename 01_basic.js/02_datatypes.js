d103f109306eb9b00e298f99fa988d08ae45e1b5
Shubham mishra <128956800+loveofcode2310@users.noreply.github.com>
2024-05-12 11:33:16 +0000

    datatypes

---
+++ 01_basic.js/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using node.js,not browser
+
+console.log(3
+    +3)  //code readability should be hight
+
+console.log("shubham")
+
+
+let name="shubham"
+let age=18
+let isLoggedIn=false
+
+//number =>2 to power 53
+// bigint
+//string => ""
+//boolean =>true/false
+//null=>standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
