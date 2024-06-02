fd0a2fe9ad13a6d6e4b3280bd879047a6b4e819d
Shubham mishra <128956800+loveofcode2310@users.noreply.github.com>
2024-06-02 06:44:14 +0000

    1to

--- 01_basic.js/03_conversion.js
+++ 01_basic.js/03_conversion.js
@@ -26,3 +26,28 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+let value =3
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1= "hello"
+let str2="shubham"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+console.log(true)
+console.log(+true);
+console.log(+"");
