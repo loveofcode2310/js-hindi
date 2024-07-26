4986b02a2165b9910a4447c1500bc119ebb923de
Shubham mishra <128956800+loveofcode2310@users.noreply.github.com>
2024-07-26 18:30:18 +0000

    frist class

---
+++ 01_basic.js/datatypes summary.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 types :String, Number,Boolearn,null,undefined,symbol,BigInt
+  const score =100
+  const scoreValue=100.3
+
+  const isLoggedIn= false
+  const outsideTemp = null
+  let userEmail;
+
+  const id= Symbol('123')
+  const anotherId =Symbol('123')
+  console.log(id == anotherId)
+ // const bigNumber=91990702317562841345n
+
+ // Reference (Non primitive)
+
+ // Array,objects,Functions
+  const heros =["shubham", "naaraj","shubham"] //Array
+  let myobj ={ //object
+    name:"shubahm kumar Mishra",
+    age:22,
+  }
+  // Funcation
+   const myFuncation =function(){
+    console.log("hello world")
+   }
+   console.log(typeof bigNumber)
+
